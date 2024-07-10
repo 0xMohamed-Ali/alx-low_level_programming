8cc746ba20be1afafcbbbefbc7753c60f789bda1
Mohamedaley <vulturepremier@gmail.com>
2024-07-10 23:01:49 +0300

    Fixing the second file

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -1,6 +1,8 @@
 #include "main.h"
 /**
- * 
+ * _print_rev_recursion - prints a string in reverse
+ * @s: string to be printed
+ * Return: void
  */
 void _print_rev_recursion(char *s)
 {
@@ -9,5 +11,5 @@ void _print_rev_recursion(char *s)
 		return;
 	}
 	_print_rev_recursion(s + 1);
-	_putchar(*s);
+	putchar(*s);
 }
