6a7e42c8ab7f2203c1e9f0c44ebe2799a4dceb65
Mohamedaley <vulturepremier@gmail.com>
2024-07-10 22:59:10 +0300

    Uplaoding 1-print_rev_recursion.c

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * 
+ */
+void _print_rev_recursion(char *s)
+{
+	if (*s == '\0')
+	{
+		putchar('\n');
+		return;
+	}
+	else
+	{
+		_print_rev_recursion(s + 1);
+		putchar(*s);
+	}
+}
--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -5,5 +5,5 @@
 #include <stdlib.h>
 
 void _puts_recursion(char *s);
-
+void _print_rev_recursion(char *s);
 #endif
