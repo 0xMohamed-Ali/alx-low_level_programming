7b645606fbc1c29132e63d3b8f7217358db30249
Mohamedaley <vulturepremier@gmail.com>
2024-07-16 16:27:44 +0300

    New commit

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -8,12 +8,13 @@
  */
 char *create_array(unsigned int size, char c)
 {
+	unsigned int i;
 	char *arr = malloc(size * sizeof(char));
 
 	if (arr == NULL)
 		return (NULL);
 
-	for (unsigned int i = 0; i < size; i++)
+	for (i = 0; i < size; i++)
 	{
 		arr[i] = c;
 	}
