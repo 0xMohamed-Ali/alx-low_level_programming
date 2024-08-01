7f2cb818cce077831059419edec5decdb65b418a
Mohamedaley <vulturepremier@gmail.com>
2024-08-01 22:49:16 +0300

    Uploading files

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,30 @@
+#include "lists.h"
+/**
+ * print_list - function to print sinlgle linked list
+ * @h: the pointer passed
+ * Return: the number of nodes
+ */
+size_t print_list(const list_t *h)
+{
+	int count = 0;
+	size_t len = 0;
+	list_t *head = (list_t *)h;
+
+	if (head == NULL)
+		return (0);
+	while (head != NULL)
+	{
+		if (head->str == NULL)
+		{
+			printf("[0] (nil)\n");
+		}
+		else
+		{
+			printf("[%d] %s\n", head->len, head->str);
+		}
+		head = head->next;
+		count++;
+	}
+	len = count;
+	return (len);
+}
